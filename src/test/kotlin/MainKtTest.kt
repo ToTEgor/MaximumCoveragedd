7996890549cda88143480de72c51426ee89085bf
Egor <ooonaliv@mail.ru>
2023-04-06 18:58:04 +0300

    correct

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -30,4 +30,68 @@ class MainKtTest {
         val res = tax(cardName = cardName, lastTransfer = lastTransfer, amount = amount)
         assertEquals(- 1,res)
     }
+
+    @Test
+    fun cardFreeVisa(){
+        val cardName = "Visa"
+        val lastTransfer = 0
+        val amount = 500
+        val res = tax(cardName = cardName, lastTransfer = lastTransfer, amount = amount)
+        assertEquals(35,res)
+    }
+
+    @Test
+    fun cardFreeVisatax(){
+        val cardName = "Visa"
+        val lastTransfer = 0
+        val amount = 55500
+        val res = tax(cardName = cardName, lastTransfer = lastTransfer, amount = amount)
+        assertEquals(416,res)
+    }
+
+    @Test
+    fun cardFreeVisataxx(){
+        val cardName = "Visax"
+        val lastTransfer = 0
+        val amount = 55500
+        val res = tax(cardName = cardName, lastTransfer = lastTransfer, amount = amount)
+        assertEquals(- 2,res)
+    }
+
+    @Test
+    fun VK(){
+        val cardName = "VK pay"
+        val lastTransfer = 20_000
+        val amount = 55500
+        val res = tax(cardName = cardName, lastTransfer = lastTransfer, amount = amount)
+        assertEquals(- 1,res)
+    }
+
+    @Test
+    fun VKpay(){
+        val cardName = "VK pay"
+        val lastTransfer = 1000
+        val amount = 5500
+        val res = tax(cardName = cardName, lastTransfer = lastTransfer, amount = amount)
+        assertEquals(0,res)
+    }
+
+    @Test
+    fun cardFreeMaestrotaxx(){
+        val cardName = "Maestro"
+        val lastTransfer = 60_000
+        val amount = 0
+        val res = tax(cardName = cardName, lastTransfer = lastTransfer, amount = amount)
+        assertEquals(0,res)
+    }
+
+    @Test
+    fun VKtax(){
+        val cardName = "VK pay"
+        val lastTransfer = 0
+        val amount = 55500
+        val res = tax(cardName = cardName, lastTransfer = lastTransfer, amount = amount)
+        assertEquals(- 1,res)
+    }
+
 }
